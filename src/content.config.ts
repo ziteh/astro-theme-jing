54d2642ab8e6d65680f4486145ae6de50c368456
ZiTe <honmonoh@gmail.com>
2026-04-04 19:01:33 +0800

    feat: remove heroImage from frontmatter

--- src/content.config.ts
+++ src/content.config.ts
@@ -13,7 +13,6 @@ const blog = defineCollection({
       // Transform string to Date object
       pubDate: z.coerce.date(),
       updatedDate: z.coerce.date().optional(),
-      heroImage: z.optional(image()),
     }),
 });
 

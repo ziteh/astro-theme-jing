2fdeadddfc72fa30ab3901eb5d8193377b584d14
ZiTe <honmonoh@gmail.com>
2026-04-04 23:46:27 +0800

    feat: add pagefind search feature

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 // @ts-check
 
+import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
-import { defineConfig } from "astro/config";
+import pagefind from "astro-pagefind";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://example.com",
-  integrations: [mdx(), sitemap()],
+  integrations: [mdx(), sitemap(), pagefind()],
 });

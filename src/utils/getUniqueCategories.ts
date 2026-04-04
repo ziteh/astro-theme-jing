a25a71f55bc67269c533f85e7ac8e064d6a010d3
ZiTe <honmonoh@gmail.com>
2026-04-04 23:09:29 +0800

    feat: add categories page

---
+++ src/utils/getUniqueCategories.ts
@@ -0,0 +1,38 @@
+import type { CollectionEntry } from "astro:content";
+import { slugifyStr } from "./slugify";
+import postFilter from "./postFilter";
+
+interface Category {
+  id: string;
+  name: string;
+  count: number;
+}
+
+const getUniqueCategories = (posts: CollectionEntry<"blog">[]): Category[] => {
+  const categoryMap = new Map<string, Category>();
+
+  posts
+    .filter(postFilter)
+    .flatMap((post) => post.data.categories)
+    .forEach((category) => {
+      const id = slugifyStr(category);
+      if (categoryMap.has(id)) {
+        // Existing
+        const existingCategory = categoryMap.get(id)!;
+        existingCategory.count += 1;
+      } else {
+        // New
+        categoryMap.set(id, {
+          id,
+          name: category,
+          count: 1,
+        });
+      }
+    });
+
+  // Sort by name
+  const categories = Array.from(categoryMap.values()).sort((a, b) => a.name.localeCompare(b.name));
+  return categories;
+};
+
+export default getUniqueCategories;

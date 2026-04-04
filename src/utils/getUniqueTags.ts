4247227e22ab8842d646ea844390b085f3dea1bb
ZiTe <honmonoh@gmail.com>
2026-04-04 21:23:07 +0800

    feat: add tags to posts page

---
+++ src/utils/getUniqueTags.ts
@@ -0,0 +1,36 @@
+import type { CollectionEntry } from "astro:content";
+import { slugifyStr } from "./slugify";
+
+interface Tag {
+  id: string;
+  name: string;
+  count: number;
+}
+
+const getUniqueTags = (posts: CollectionEntry<"blog">[]): Tag[] => {
+  const tagMap = new Map<string, Tag>();
+
+  posts
+    .flatMap((post) => post.data.tags)
+    .forEach((tag) => {
+      const id = slugifyStr(tag);
+      if (tagMap.has(id)) {
+        // Existing
+        const existingTag = tagMap.get(id)!;
+        existingTag.count += 1;
+      } else {
+        // New
+        tagMap.set(id, {
+          id,
+          name: tag,
+          count: 1,
+        });
+      }
+    });
+
+  // Sort by name
+  const tags = Array.from(tagMap.values()).sort((a, b) => a.name.localeCompare(b.name));
+  return tags;
+};
+
+export default getUniqueTags;

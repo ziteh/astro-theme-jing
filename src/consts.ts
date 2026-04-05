005b9db502bded209118769469fd7e5d89cb872e
ZiTe <honmonoh@gmail.com>
2026-04-05 08:52:49 +0800

    feat: update consts

--- src/consts.ts
+++ src/consts.ts
@@ -1,11 +1,12 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
+export const SITE = {
+  title: "Astro Jing",
+  description: "An Astro theme for blogging.",
+  author: "Your Name", // 君の名は ~
 
-export const SITE_TITLE = "Astro Blog";
-export const SITE_DESCRIPTION = "Welcome to my website!";
+  lang: "en", // BCP 47 language tag
+  langOg: "en_US", // Open Graph locale tag, format language_TERRITORY (see https://ogp.me/#optional)
+  timezone: "UTC", // IANA time zone name (see https://en.wikipedia.org/wiki/List_of_tz_database_time_zones)
 
-// BCP 47 language tag
-export const LANG = "en";
-
-export const POSTS_PER_HOMEPAGE = 3;
-export const POSTS_PER_PAGE = 5;
+  postsPerHomepage: 3,
+  postsPerPage: 5,
+} as const;
--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,13 +1,13 @@
 import { getCollection } from "astro:content";
 import rss from "@astrojs/rss";
-import { SITE_DESCRIPTION, SITE_TITLE } from "../consts";
+import { SITE } from "../consts";
 import postFilter from "../utils/postFilter";
 
 export async function GET(context) {
   const posts = await getCollection("blog");
   return rss({
-    title: SITE_TITLE,
-    description: SITE_DESCRIPTION,
+    title: SITE.title,
+    description: SITE.description,
     site: context.site,
     items: posts.filter(postFilter).map((post) => ({
       ...post.data,

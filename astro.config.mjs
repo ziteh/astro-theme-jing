d6b858407da8f4a99e19d38107f870e666aaea88
ZiTe <honmonoh@gmail.com>
2026-04-05 08:07:28 +0800

    feat: add expressive code

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,42 @@
 
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
+import { pluginLineNumbers } from "@expressive-code/plugin-line-numbers";
 import { defineConfig } from "astro/config";
+import expressiveCode from "astro-expressive-code";
 import pagefind from "astro-pagefind";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://example.com",
-  integrations: [mdx(), sitemap(), pagefind()],
+  integrations: [
+    expressiveCode({
+      plugins: [pluginLineNumbers()],
+      themes: ["one-dark-pro", "min-light"],
+      defaultProps: {
+        wrap: false,
+        showLineNumbers: false,
+        overridesByLang: {
+          "bash,cmd,powershell,ps,sh,shell,zsh": { frame: "none" },
+        },
+      },
+      styleOverrides: {
+        codeFontFamily: "var(--font-mono), var(--font-emoji)",
+        uiFontFamily: "var(--font-sans), var(--font-emoji)",
+        borderWidth: "0",
+        textMarkers: {
+          backgroundOpacity: "33%",
+          inlineMarkerBorderWidth: "0.1px",
+        },
+        frames: {
+          editorTabBarBackground: "transparent",
+          frameBoxShadowCssValue: "transparent",
+          tooltipSuccessBackground: "#6b7280",
+        },
+      },
+    }),
+    mdx(),
+    sitemap(),
+    pagefind(),
+  ],
 });

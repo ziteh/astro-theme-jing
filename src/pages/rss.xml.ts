4b8951e530d36c12da0aff48be8f0e0c4312569c
ZiTe <honmonoh@gmail.com>
2026-04-06 18:23:17 +0800

    refactor: update RSS feed generation endpoint to TS

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -1,16 +1,21 @@
 import rss from "@astrojs/rss";
+import type { APIRoute } from "astro";
 import { SITE } from "@/consts";
 import getBlogPosts from "@/utils/getPosts";
 
-export async function GET(context) {
+export const GET: APIRoute = async ({ site }) => {
+  if (!site) {
+    throw new Error("Site URL is not defined.");
+  }
+
   const posts = await getBlogPosts();
   return rss({
     title: SITE.title,
     description: SITE.description,
-    site: context.site,
+    site,
     items: posts.map((post) => ({
       ...post.data,
-      link: `/posts/${post.id}/`,
+      link: `/posts/${post.id}`,
     })),
   });
-}
+};
